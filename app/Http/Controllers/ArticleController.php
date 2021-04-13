721ae1601d77007062616c6e7dafe03bd52ece49
= <=>
2021-04-14 04:52:56 +0700

    menambhkan function cetak pfdf dalam controler

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use PDF;
 use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -106,5 +106,10 @@ class ArticleController extends Controller
     public function destroy(Article $article)
     {
         //
+    }
+    public function cetak_pdf(){
+        $articles = Article::all();
+        $pdf = PDF::loadview('articles.articles_pdf',['articles'=>$articles]);
+        return $pdf->stream();
     }
 }
